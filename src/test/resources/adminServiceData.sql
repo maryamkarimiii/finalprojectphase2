45a9eb3981dfb1f98f1a90b2a21688a0e885c003
maryam karimi <maryamkarimi1376.71@gmail.com>
2023-02-04 13:59:26 +0330

    add adminServicedata sql file to resours

---
+++ src/test/resources/adminServiceData.sql
@@ -0,0 +1,4 @@
+insert into expert (email, first_name, last_name, password, phone_number, role, username,
+                    expert_registration_status, total_score)
+values ('maryam12@gmail.com', 'maryam', 'karimi', '123456Mk', '09100321487', 'EXPERT', 'maryam12@gmail.com',
+        'NEW',0.0);
